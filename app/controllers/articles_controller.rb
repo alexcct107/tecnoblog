3f62c37131897a5aaacc3c88522f691153da2ca4
alexcct <ivanalejandro@MacBook-Air-de-Ivan.local>
2020-02-26 16:25:32 -0300

    updated images and sections

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
    #GET /articles/new
    def new
     @article = Article.new
+    @categories = Category.all
    end
 
    def edit
@@ -23,7 +24,8 @@ class ArticlesController < ApplicationController
    
    #POST /articles
    def create      
-    @article = current_user.articles.new(article_params)
+    @article = current_user.articles.new(article_params) 
+    @article.categories = params[:categories]
    if @article.save
     redirect_to @article
     else
@@ -54,7 +56,7 @@ end
    end
 
    def article_params
-       params.require(:article).permit(:title, :body, :cover)
+       params.require(:article).permit(:title, :body, :cover, :categories)
 
    end
 end
